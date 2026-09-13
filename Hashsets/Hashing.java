7f6c773025c94441d5d52095f4d940ba949d4574
Agrima Saxena <agrimalc@gmail.com>
2026-09-14 02:49:06 +0530

    Hashing

--- Hashsets/Hashing.java
+++ Hashsets/Hashing.java
@@ -1,3 +1,23 @@
+import java.util.HashSet;
 public class Hashing {
-    
+    public static void main(String args[])
+    {
+        HashSet<Integer> set=new HashSet<>();// ArrayList<Integer> list=new ArrayList<>();
+    //Insert values in HashSet
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(5);//list.add(5);
+        System.out.println(set);
+    //Search/contains value in HashSet
+        if(set.contains(1))
+        {
+            System.out.println("Set contains 1");
+        }
+        if(!set.contains(6))
+        {
+            System.out.println("Set does not contain 6");
+        }
+    }
 }
