cbcf63dd4aa23c0fd8dcac0967b32cdb5782dd8b
Agrima Saxena <agrimalc@gmail.com>
2026-09-16 03:12:55 +0530

    adding HashingIterations and use of Iterators

---
+++ Hashsets/HashingIterations.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Iterator;
+public class HashingIterations
+{
+    public static void main(String args[])
+    {
+        HashSet <Integer> set=new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        System.out.println(set);
+        //Iterator
+        Iterator it =set.iterator();
+        while(it.hasNext())
+        {
+            System.out.println(it.next());
+        }
+    }
+}
